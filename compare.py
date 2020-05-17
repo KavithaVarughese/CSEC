740d417720f5f7266ae707f98dda251ca536c515
Codebreaker <37763424+FatimaBasheer@users.noreply.github.com>
2020-05-17 14:40:13 +0530

    Storing DES encryption time in variable 'javatime'

--- compare.py
+++ compare.py
@@ -4,7 +4,17 @@ import subprocess
 x = os.popen('python bf.py -e files/001.xml out1').read()
 print(x)
 
-y = os.system('java des files/001.xml out2')
-print(y)
+
+# storing DES encryption time in variable javatime
+# calling java program
+javaout = subprocess.Popen(['java','des', '../test.xml', '../encxml'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+stdout,stderr = out.communicate();
+stdout = str(stdout)
+#obtaining digits from the byte format of the returned value
+javatime = ''
+for c in stdout:
+    if c.isdigit():
+        javatime=javatime+c
+# print("Encryption Time for DES : " + javatime)
 
 print("DONE")
